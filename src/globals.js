22ecbc71d246cb03ec1f9878d5a38e9f550f6b2e
Trace <tracedelange@me.com>
2021-10-25 17:06:32 -0500

    tweaking action cable config for production

--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,6 @@
 export const baseURL = process.env.REACT_APP_BASE_URL;
 
-export const websocket = 'ws://monster-audience-web.herokuapp.com/cable'
+export const websocket = process.env.REACT_APP_WEBSOCKET
 
 export const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
